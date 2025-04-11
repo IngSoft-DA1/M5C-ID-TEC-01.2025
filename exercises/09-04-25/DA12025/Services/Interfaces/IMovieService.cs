f7a95b9753768762400a0291df897ea3703fa4d5
marcos <marcosbuydid@gmail.com>
2025-04-11 20:04:39 -0300

    add blazor excercise

---
+++ exercises/09-04-25/DA12025/Services/Interfaces/IMovieService.cs
@@ -0,0 +1,11 @@
+﻿using Domain;
+
+namespace Services.Interfaces
+{
+    public interface IMovieService
+    {
+        List<Movie> GetMovies();
+        void AddMovie(Movie movie);
+        Movie GetMovie(string title);
+    }
+}
---
+++ exercises/09-04-25/DA12025/Services/MovieService.cs
@@ -0,0 +1,50 @@
+﻿using Domain;
+using DataAccess;
+using Services.Interfaces;
+
+namespace Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly InMemoryDatabase _inMemoryDatabase;
+
+        public MovieService(InMemoryDatabase inMemoryDatabase)
+        {
+            _inMemoryDatabase = inMemoryDatabase;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            ValidateUniqueTitle(movie.Title);
+
+            _inMemoryDatabase.Movies.Add(movie);
+        }
+
+        public List<Movie> GetMovies()
+        {
+            return _inMemoryDatabase.Movies;
+        }
+
+        public Movie GetMovie(string title)
+        {
+            Movie? movie = _inMemoryDatabase.Movies.FirstOrDefault(movie => movie.Title == title);
+            if (movie == null)
+            {
+                throw new ArgumentException("Cannot find movie with this title");
+            }
+
+            return movie;
+        }
+
+        private void ValidateUniqueTitle(String title)
+        {
+            foreach (var movie in _inMemoryDatabase.Movies)
+            {
+                if (movie.Title == title)
+                {
+                    throw new ArgumentException("There`s a movie already defined with that title");
+                }
+            }
+        }
+    }
+}
