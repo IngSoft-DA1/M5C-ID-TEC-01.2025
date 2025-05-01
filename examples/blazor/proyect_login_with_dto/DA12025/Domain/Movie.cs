bd04d4c28de8542e2766ffe9ad833d2bae280b0e
marcos <marcosbuydid@gmail.com>
2025-04-30 23:23:47 -0300

    add login proyect with dto

---
+++ examples/blazor/proyect_login_with_dto/DA12025/Domain/Movie.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain;
+
+public class Movie
+{
+    public string Title { get; set; }
+    public string Director { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public int Budget { get; set; }
+
+    public Movie(string title, string director, DateTime releaseYear, int budget)
+    {
+        Title = title;
+        Director = director;
+        ReleaseDate = releaseYear;
+        Budget = budget;
+    }
+}
---
+++ examples/blazor/proyect_login_with_dto/DA12025/Services/Models/MovieDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Services.Models
+{
+    public class MovieDTO
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Budget { get; set; }
+    }
+}
