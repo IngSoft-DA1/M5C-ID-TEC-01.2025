00dd9e9e35d6b51a882eb2480c8d82c83e31e8ca
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2025-05-30 11:30:47 -0300

    feature: add EF example

---
+++ examples/entity-framework/db-connection-and-repository-update/DA12025/DataAccess/MovieRepository.cs
@@ -0,0 +1,54 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public class MovieRepository
+{
+    private readonly AppDbContext _context;
+
+    public MovieRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Add(Movie movie)
+    {
+        _context.Movies.Add(movie);
+        _context.SaveChanges();
+    }
+
+    public ICollection<Movie> GetMovies()
+    {
+        return _context.Movies.ToList();
+    }
+
+    public Movie GetMovie(int id)
+    {
+        return _context.Movies.Find(id);
+    }
+
+    public void Update(Movie movieWithUpdatedData)
+    {
+        Movie movieFound = _context.Movies.Find(movieWithUpdatedData.Id);
+        _context.Movies.Update(movieWithUpdatedData);
+        _context.Entry(movieWithUpdatedData).State = EntityState.Modified; 
+        _context.SaveChanges();
+    }
+
+    public void Remove(Movie movie)
+    {
+        //Este ejemplo contiene manejo de exceptions, lo mismo debe ser realizado en todos los metodos de cada repository
+        try
+        {
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+
+}
